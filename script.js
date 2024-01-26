e273981e2184eaf5987fdb4f5609d1a7fd5ca39b
Yashas-K-N <147701279+Yashas-K-N@users.noreply.github.com>
2024-01-26 21:22:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+      return 2*str[i].charAtCode(0) ? null:str[i].charAtCode(0);
+
+
+
+	
+	
 }
 const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+/*((firstNonRepeatedChar(input));
