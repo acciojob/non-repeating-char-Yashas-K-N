b3dafb798f9b9e0985f8de94b50ee673fb3b4ade
Yashas-K-N <147701279+Yashas-K-N@users.noreply.github.com>
2024-02-25 22:12:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,12 @@ function firstNonRepeatedChar(str) {
  // Write your code here
 	let str =str.toLowerCase();
 	for(let i=0; i<=str.length; i++ ){
-	if(str.indexOfstr[i]===str.lastindexOfstr[i]){
+	if(str.indexOf(str[i])===str.lastIndexOf(str[i])){
     return str[i];
 	}
 	
 }
-return null
+return null;
 }
 let output =firstNonRepeatedChar();
 console.log(output);
