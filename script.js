97ac1bd3eee1afe52e39627eeaf331748c7db916
Yashas-K-N <147701279+Yashas-K-N@users.noreply.github.com>
2024-02-25 19:32:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	if(str[i].charAtCode(0)>"a" && str[i].charAtCode(0)<"b"){
       return 2*str[i].charAtCode(0) ? "null":"str[i].charAtCode(0);"
-
+	}
 
 
 	
